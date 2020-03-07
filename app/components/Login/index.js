f3a382fefefdbda8cc6389c50b8d8e63d8f178f7
Hassan Adaminejad <hassan.drykiss@gmail.com>
2020-03-07 16:08:36 +0330

    add login register

---
+++ app/components/Login/index.js
@@ -0,0 +1,195 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
+import { Modal, Form, Input, Button, Tabs } from 'antd';
+import { UserOutlined, LockOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons';
+import request from '../../utils/request';
+import { Wrapper } from './style';
+// import BannerRight from '../../images/banner/banner.png';
+const { TabPane } = Tabs;
+
+function Login(props) {
+  const { visible, cancel } = props;
+
+  const {
+    getFieldDecorator,
+    validateFields
+  } = props.form;
+
+  const handleLogin = (value) => {
+    const { username, password } = value;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://vision-idea.com/backend/events/user/signin";
+    request
+      .post(proxyurl + url, { signfactor: username, password })
+      .then(res => {
+        // eslint-disable-next-line no-console
+        console.log(res)
+      })
+      // eslint-disable-next-line no-console
+      .catch(_ => console.log(_));
+
+  }
+  const handleRegister = (value) => {
+    const { email, password, cell } = value;
+    const url = "https://vision-idea.com/backend/events/user/signup";
+    request
+      .get(`${url}?cell=${cell}&email=${email}&password=${password}`)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(_ => console.log(_));
+
+  }
+
+  const handleSubmitLogin = e => {
+    e.preventDefault();
+    validateFields((err, values) => {
+      console.log(values)
+      if (!err) {
+        handleLogin(values)
+      }
+    });
+  };
+
+  const handleSubmitRegister = e => {
+    e.preventDefault();
+    validateFields((err, values) => {
+      // eslint-disable-next-line no-console
+      console.log(values)
+      if (!err) {
+        handleRegister(values)
+      }
+    });
+  };
+
+  const renderLoginHtml = () => (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onSubmit={handleSubmitLogin}
+    >
+
+      <Form.Item >
+        {getFieldDecorator('username', {
+          rules: [{ required: true, message: 'Please input your Username!' }],
+
+        })(
+          <Input
+            style={{ width: '100%' }}
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />,
+        )}
+      </Form.Item>
+      <Form.Item >
+        {getFieldDecorator('password', {
+          rules: [{ required: true, message: 'Please input your Password!' }],
+
+        })(
+          <Input
+            style={{ width: '100%' }}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Password"
+            type="password"
+          />,
+        )}
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+
+  const renderRegisterHtml = () => (
+    <Form
+      name="register"
+      className="login-form"
+      onSubmit={handleSubmitRegister}
+    >
+
+      <Form.Item >
+        {getFieldDecorator('email', {
+          rules: [{ required: true, message: 'Please input your email!' }],
+
+        })(
+          <Input
+            style={{ width: '100%' }}
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />,
+        )}
+      </Form.Item>
+      <Form.Item >
+        {getFieldDecorator('cell', {
+          rules: [{ required: true, message: 'Please input your cell!' }],
+
+        })(
+          <Input
+            style={{ width: '100%' }}
+            prefix={<PhoneOutlined className="site-form-item-icon" />}
+            placeholder="Cell phone"
+          />,
+        )}
+      </Form.Item>
+      <Form.Item >
+        {getFieldDecorator('password', {
+          rules: [{ required: true, message: 'Please input your Password!' }],
+
+        })(
+          <Input
+            style={{ width: '100%' }}
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Password"
+            type="password"
+          />,
+        )}
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Sign Up
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+
+
+  return (
+    <Wrapper>
+      <Modal
+        title="Login"
+        style={{ top: 20 }}
+        // visible={this.state.modal1Visible}
+        visible={visible}
+        // onOk={() => this.setModal1Visible(false)}
+        onCancel={() => cancel()}
+      >
+        <Tabs defaultActiveKey="1" size='large' >
+          <TabPane tab="Login" key="1">
+            {renderLoginHtml()}
+
+          </TabPane>
+          <TabPane tab="Register" key="2">
+            {renderRegisterHtml()}
+          </TabPane>
+        </Tabs>
+      </Modal>
+    </Wrapper>
+  );
+}
+
+Login.propTypes = {
+  visible: PropTypes.bool,
+  cancel: PropTypes.func,
+  form: PropTypes.any,
+};
+
+export default Form.create()(Login);
--- app/components/Menu/index.js
+++ app/components/Menu/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { PropTypes } from 'prop-types';
 
 import { Menu } from 'antd';
+import Login2 from "../Login";
 
 
 
-function MenuComponent ({ pathname }) {
+function MenuComponent({ pathname }) {
   const menu = [
     {
       id: 1,
@@ -24,30 +25,40 @@ function MenuComponent ({ pathname }) {
       id: 3,
       route: '/my-ticket',
       title: 'MY TICKETS',
-    },{
+    }, {
       id: 4,
       route: '/sell-tickets',
       title: 'SELL TICKETS',
-    },
+    }, {
+      id: 5,
+      // route: '/login',
+      title: 'Login',
+      onClick: () => setVisibleLogin(true)}
   ];
   const activeMenu = menu.find(item => item.route === pathname);
+  const [visibleLogin,setVisibleLogin] = useState(false);
+  const handleCancelVisibleLogin = () => {
+    setVisibleLogin(false)
+  }
   return (
-
-    <Menu
-      onClick={e => console.log(e)}
-      defaultSelectedKeys={[`${activeMenu ? activeMenu.id : '1'}`]}
-      mode='horizontal'
-      style={{borderBottom: 'none'}}
-    >
-      {menu.map(item => (
-        <Menu.Item key={`${item.id}`}>
-          <Link to={item.route}>
-            {/* <Icon type={item.icon} /> */}
-            <span>{item.title}</span>
-          </Link>
-        </Menu.Item>
-      ))}
-    </Menu>
+    <>
+      <Login2 visible={visibleLogin} cancel={handleCancelVisibleLogin}/>
+      <Menu
+        // onClick={e => console.log(e)}
+        defaultSelectedKeys={[`${activeMenu ? activeMenu.id : '1'}`]}
+        mode='horizontal'
+        style={{ borderBottom: 'none' }}
+      >
+        {menu.map(item => (
+          <Menu.Item key={`${item.id}`} onClick={item.onClick || null}>
+            <Link to={item.route}>
+              {/* <Icon type={item.icon} /> */}
+              <span>{item.title}</span>
+            </Link>
+          </Menu.Item>
+        ))}
+      </Menu>
+    </>
   );
 }
 
